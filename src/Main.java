1b5e2101894d95c61d6d541e8cd0c19297661c32
kaqui95 <karstentaquino@gmail.com>
2025-01-22 14:43:19 -0800

    01/22/25

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class    Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello Idea, 01/22/2025!");
+        System.out.printf("Hello Idea, lets get out of here early 01/22/2025!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
